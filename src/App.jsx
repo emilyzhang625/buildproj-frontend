baa5129c037a19cd9f027d7509d223978c22b0fc
Soha Baig <soha.baig94@gmail.com>
2024-11-09 18:46:14 -0800

    front end option 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,63 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [grams, setGrams] = useState(0);
-  const min = 0;
-  const max = 3;
-
+  const [grams, setGrams] = useState(0); 
+  const [isCustom, setIsCustom] = useState(false); 
+  
   const handleSubmit = () => {
     console.log(grams);
     console.log("making matcha");
   };
 
+  const handleCustomInput = (e) => {
+    let value = parseFloat(e.target.value);
+    if (value > 4) {
+      value = 4;
+    } else if (value < 0) {
+      value = 0;
+    }
+    setGrams(value); 
+  };
+
+  const handleShowCustomInput = () => {
+    setIsCustom(!isCustom);
+  };
+
+  const handlePresetGrams = (value) => {
+    setGrams(value);
+    setIsCustom(false); 
+  };
+
   return (
     <div>
-      <input
-        type="number"
-        value={grams}
-        min={min}
-        max={max}
-        step="0.2"
-        onChange={(e) => {
-          setGrams(e.target.value);
-        }}
-        onKeyDown={(event) => {
-          if (event.key !== "ArrowUp" && event.key !== "ArrowDown") {
-            event.preventDefault();
-          }
-        }}
-      ></input>
-      <button onClick={handleSubmit}>Go!</button>
+      <h1>Matcha Maker</h1>
+      <div className="matcha-buttons-container">
+        <button onClick={() => handlePresetGrams(1)}>1g</button>
+        <button onClick={() => handlePresetGrams(2)}>2g</button>
+        <button onClick={() => handlePresetGrams(3)}>3g</button>
+        <button onClick={() => handlePresetGrams(4)}>4g</button>
+        <button className="show-custom" onClick={handleShowCustomInput}>
+          Custom
+        </button>
+      </div>
+      {isCustom && (
+        <div className="custom-input-container">
+          <input
+            type="number"
+            value={grams}
+            min="0"
+            max="4"
+            step="0.1"
+            onChange={handleCustomInput}
+            placeholder="Enter custom value"
+          />
+        </div>
+      )}
+      <button className="go-button" onClick={handleSubmit}>
+        Go!
+      </button>
       <div>
-        Please enter the amount of matcha powder in grams within the respective
-        range of 0 to 3 grams.
+        <p>Amount of matcha: {grams}g</p>
       </div>
     </div>
   );
